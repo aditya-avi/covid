abc2c6371b3b421ab60d1f765886ded311ad5071
Aditya Avinash <avinash.aditya.mlps@gmail.com>
2020-06-05 01:09:40 +0530

    Updates

---
+++ precache-manifest.75cd916d553d1e0b5fb7e61336f17cb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be22dc36d8c286d45f4d2f7022f36a5",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "59adec270f7b61eb98d8",
+    "url": "/covid/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e61226038218a00d567b",
+    "url": "/covid/static/css/main.f42c9281.chunk.css"
+  },
+  {
+    "revision": "59adec270f7b61eb98d8",
+    "url": "/covid/static/js/2.ceb3309b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid/static/js/2.ceb3309b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61226038218a00d567b",
+    "url": "/covid/static/js/main.a4fc3346.chunk.js"
+  },
+  {
+    "revision": "dd7f6c094f0f5e36b6b3",
+    "url": "/covid/static/js/runtime-main.ed7b7e5f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid/static/media/img.d7265326.png"
+  }
+]);
